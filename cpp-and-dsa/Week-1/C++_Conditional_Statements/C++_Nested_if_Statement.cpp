61022bd36cef71a051338832a2530322e868907e
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-19 16:45:28 +0530

    commit

--- cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_Nested_if_Statement.cpp
+++ cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_Nested_if_Statement.cpp
@@ -7,4 +7,23 @@ int main() {
     if (a > b) {
         cout << "True" << endl;
     }
+
+    // When a number of if blocks are nested inside another if block,
+    // it is known as a nested if statement.
+    int c = 20, d = 10, e = 2;
+    if (c > d) {
+        if (c > e) {
+            cout << "c is the largest number" << endl;
+        }
+    }
+
+    int f = 20, g = 10, h = 30;
+    if (f == 20) {
+        if (g == 10) {
+            if (h == 30) {
+                cout << "f, g, and h have the correct values." << endl;
+            }
+        }
+    }
+
 }
