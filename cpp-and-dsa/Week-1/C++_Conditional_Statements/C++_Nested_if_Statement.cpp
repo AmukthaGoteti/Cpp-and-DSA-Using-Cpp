bb99b9411691d66357f28465947d3c77f1aa99b9
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-19 16:37:55 +0530

    cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_Nested_if_Statement.cpp

---
+++ cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_Nested_if_Statement.cpp
@@ -0,0 +1,10 @@
+// C++ Program to demonstrate Nested if Statement
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 6, b = 5;
+    if (a > b) {
+        cout << "True" << endl;
+    }
+}
