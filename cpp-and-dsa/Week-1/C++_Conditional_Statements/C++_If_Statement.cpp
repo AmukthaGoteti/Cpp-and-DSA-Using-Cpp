0cfb68f73eaa25a715fed1a058b1cee35e9e2aac
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-19 16:15:26 +0530

    If statement

---
+++ cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_If_Statement.cpp
@@ -0,0 +1,15 @@
+// The C++ if Statement is the most basic decision-making statement.
+// It allows you to execute a block of code only if a specified condition is true.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 10;
+    // If statement
+    if (i < 15) {
+        cout << "10 is less than 15" << endl;
+    }
+
+    return 0;
+}
