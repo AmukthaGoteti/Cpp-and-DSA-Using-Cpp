ee2874842560d6d651af0e50f66f9554f5e0316b
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-20 20:07:20 +0530

    commit

---
+++ cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_Loops.cpp
@@ -0,0 +1,13 @@
+// In C++ programming, sometimes there is a need to perform some operation more than once or n number of times.
+// In such cases, loops come into play, allowing users to repeatedly execute a block of statements any number of times.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        cout << "Hello, World!" << endl;
+    }
+    
+    return 0;
+}
