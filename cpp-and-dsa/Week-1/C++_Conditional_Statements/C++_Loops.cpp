dc0fc1a420e7eb34c4e9368e6450b63fc99c265b
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-20 20:11:44 +0530

    commit

--- cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_Loops.cpp
+++ cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_Loops.cpp
@@ -8,6 +8,14 @@ int main() {
     for (int i = 0; i < 5; i++) {
         cout << "Hello, World!" << endl;
     }
-    
+    // C++ provides three types of loops that works the same, but are preferred in different scenarios:
+    // 1. for loop
+    // The for loop is an entry-controlled loop, which means that it checks wheather the test condition is true 
+    // before excuting the statement inside it.
+    // Program to print numbers from 1 to 5 using for loop
+    for (int num = 1; num <=5; num++) {
+        cout << num << " ";
+    }
+    cout << endl;
     return 0;
 }
