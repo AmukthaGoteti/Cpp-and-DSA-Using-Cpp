4841b2130e507dd497829ed458727831606ecc3b
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-25 15:44:53 +0530

    commit

---
+++ cpp-and-dsa/Week-2/C++_Stack.cpp
@@ -0,0 +1,19 @@
+/*
+    Stack container follows LIFO (Last In First Out) order 
+    of insertion and deletion. 
+    It means that most recently inserted element is removed first 
+    and the first inserted element will be removed last. 
+    This is done by inserting and deleting elements 
+    at only one end of the stack which is generally called 
+    the top of the stack.
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    stack<int> st;
+    st.push(10);
+    st.push(5);
+    cout << "Top element: " << st.top() << endl;
+}
