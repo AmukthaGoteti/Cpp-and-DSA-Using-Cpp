7a979d7766dd49859e9e7795e3b0530b7a65eb39
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-03 14:16:14 +0530

    commit

--- cpp-and-dsa/Week-3/Easy/Practice_Problem_1.cpp
+++ cpp-and-dsa/Week-3/Easy/Practice_Problem_1.cpp
@@ -10,3 +10,19 @@
         Output: [5]
         Explanation: We only have two elements so max of 5 and 5 is 5 only.
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // We compare adjacent pairs, so loop runs till n-1
+    for (int i = 0; i < n - 1; i++) {
+        cout << max(arr[i], arr[i + 1]) << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
