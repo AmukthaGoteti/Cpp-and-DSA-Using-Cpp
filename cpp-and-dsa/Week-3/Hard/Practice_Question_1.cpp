921b2c9b4d9f4a7935cc35068d386b947dbdc6fc
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-13 16:24:32 +0530

    commit

--- cpp-and-dsa/Week-3/Hard/Practice_Question_1.cpp
+++ cpp-and-dsa/Week-3/Hard/Practice_Question_1.cpp
@@ -20,36 +20,70 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> fourSum(vector<int>& nums, int target) {
+vector<vector<int>> fourSum(vector<int>& nums, int target) 
+{
     sort(nums.begin(), nums.end());
     vector<vector<int>> res;
     int n = nums.size();
-    for(int i = 0; i < n; i++){
-        if(i > 0 && nums[i] == nums[i-1]) continue;
-        for(int j = i + 1; j < n; j++){
-            if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+    for(int i = 0; i < n; i++)
+    {
+        if(i > 0 && nums[i] == nums[i-1]) 
+        {
+            continue;
+        }
+        for(int j = i + 1; j < n; j++)
+        {
+            if(j > i + 1 && nums[j] == nums[j - 1]) 
+            {
+                continue;
+            }
             long long t = (long long)target - nums[i] - nums[j];
             int l = j + 1, r = n - 1;
-            while(l < r){
+            while(l < r)
+            {
                 long long sum = nums[l] + nums[r];
-                if(sum == t){
+                if(sum == t)
+                {
                     res.push_back({nums[i], nums[j], nums[l], nums[r]});
-                    while(l < r && nums[l] == nums[l + 1]) l++;
-                    while(l < r && nums[r] == nums[r - 1]) r--;
+                    while(l < r && nums[l] == nums[l + 1]) 
+                    {
+                        l++;
+                    }
+                    while(l < r && nums[r] == nums[r - 1]) 
+                    {
+                        r--;
+                    }
                     l++; r--;
-                } else if(sum < t) l++;
-                else r--;
+                } else if(sum < t) 
+                {
+                    l++;
+                }
+                else 
+                {
+                    r--;
+                }
             }
         }
     }
     return res;
 }
 
-int main(){
-    int n, target; cin >> n >> target;
+int main()
+{
+    int n, target; 
+    cin >> n >> target;
     vector<int> nums(n);
-    for(int &x : nums) cin >> x;
+    for(int &x : nums) 
+    {
+        cin >> x;
+    }
     auto ans = fourSum(nums, target);
-    if(ans.empty()) cout << "No solution found\n";
-    else for(auto &v : ans) cout << "["<<v[0]<<", "<<v[1]<<", "<<v[2]<<", "<<v[3]<<"]\n";
+    if(ans.empty()) 
+    {
+        cout << "No solution found\n";
+    }
+    else 
+    {
+        for(auto &v : ans) cout << "["<<v[0]<<", "<<v[1]<<", "<<v[2]<<", "<<v[3]<<"]\n";
+    }
 }
