39cfd28c63ca15821ff8ef288e9a1f60ed9c984b
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-16 12:51:39 +0530

    commit

---
+++ cpp-and-dsa/Week-4/Two_pointers_variation_and_Sliding_Window/Leetcode_Discussion_Resources/Summary_of_Sliding_Window_Patterns_for_Subarray_or_Substring/Minimum_Window_Substring.cpp
@@ -0,0 +1,5 @@
+/*
+    Idea of the Problem Statement:
+        We track how many required characters from t are still missing in the current window, 
+        and when that count reaches zero, the window contains all of t.
+*/
