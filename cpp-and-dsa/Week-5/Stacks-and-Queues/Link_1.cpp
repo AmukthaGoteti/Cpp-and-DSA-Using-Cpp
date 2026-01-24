c2020794315f3a03963c3e221624ff10b0766b1e
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-24 17:13:42 +0530

    commit

---
+++ cpp-and-dsa/Week-5/Stacks-and-Queues/Link_1.cpp
@@ -0,0 +1,4 @@
+/*
+    Stack involves LIFO(Last In First Out) concept.
+    The last element inserted is the first to be popped out. It means both insertion and deletion operations happen at one end only.
+*/
