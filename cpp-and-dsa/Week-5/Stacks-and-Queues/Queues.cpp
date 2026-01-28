b11f7fb195cdbd8257c199e08959e986e3c799bf
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-28 17:04:55 +0530

    commit

---
+++ cpp-and-dsa/Week-5/Stacks-and-Queues/Queues.cpp
@@ -0,0 +1,17 @@
+/*
+    A queue stores multiple elements in a specific order, 
+    called FIFO.
+    FIFO stands for First In First Out.
+    The element that is added first to the queue will be the
+    first one to be removed from the queue.
+    To use queue, you need to include the <queue> header file.
+*/
+
+#include <iostream>
+#include <queue> // Include the queue header file
+using namespace std;
+
+int main() {
+    // Create a queue of stings called 'cars'
+    queue<string> cars;
+}
