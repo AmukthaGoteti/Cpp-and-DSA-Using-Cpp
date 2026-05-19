b63da7572e6ffc2701318338354f21b1daa3d6b6
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-05-19 19:18:06 +0530

    commit

--- leetcode/easy/Minimum_Common_Value.cpp
+++ leetcode/easy/Minimum_Common_Value.cpp
@@ -16,30 +16,23 @@ using namespace std;
 
 int main() {
     int n, m;
-
     // Input sizes
     cin >> n >> m;
-
     vector<int> nums1(n);
     vector<int> nums2(m);
-
     // Input elements of nums1
     for (int i = 0; i < n; i++) {
         cin >> nums1[i];
     }
-
     // Input elements of nums2
     for (int j = 0; j < m; j++) {
         cin >> nums2[j];
     }
-
     int minimum_common_value = -1;
-
     // Find common values
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (nums1[i] == nums2[j]) {
-
                 // Update minimum common value
                 if (minimum_common_value == -1 || nums1[i] < minimum_common_value) {
                     minimum_common_value = nums1[i];
@@ -47,9 +40,7 @@ int main() {
             }
         }
     }
-
     // Output result
     cout << minimum_common_value << endl;
-
     return 0;
 }
