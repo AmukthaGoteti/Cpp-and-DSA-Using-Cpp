322233ad50cb386fa61e6f7f30cabca56b956f0d
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-06-21 14:12:53 +0530

    commit

--- cpp-and-dsa/Week-1/C++_Data_Types.cpp
+++ cpp-and-dsa/Week-1/C++_Data_Types.cpp
@@ -60,7 +60,8 @@ int main() {
     /*
         Floating-point data type is used to store numbers with decimal points. 
         The keyword used to define floating-point numbers is float.
-        Its size is 4 bytes (on 64-bit systems) and can store values in the range from 1.2e-38 to 34e+38.
+        Its size is 4 bytes (on 64-bit systems) 
+        and can store values in the range from 1.2e-38 to 34e+38.
     */
 
     // Floating point variable with decimal value
