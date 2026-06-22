e2a0da1e29f1b4981f1880a93df89b1e852a300a
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-06-22 23:56:22 +0530

    commit

--- leetcode/easy/Maximum_Number_of_Balloons.cpp
+++ leetcode/easy/Maximum_Number_of_Balloons.cpp
@@ -8,3 +8,22 @@
     Difficulty: Easy
     URL: https://leetcode.com/problems/maximum-number-of-balloons/description/?envType=daily-question&envId=2026-06-22
 */
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    string text;
+    cout << "Enter the text: ";
+    cin >> text;
+    unordered_map<char, int> charCount;
+    for (char c : text) {
+        if (c == 'b' || c == 'a' || c == 'l' || c == 'l' || c == 'o' || c == 'o'  || c == 'n') {
+            charCount[c]++;
+        }
+    }
+    int maxBalloons = min({charCount['b'] / 1, charCount['a'] / 1, charCount['l'] / 2, charCount['o'] / 2, charCount['n'] / 1});
+    cout << "Maximum number of instances of 'balloon' that can be formed: " << maxBalloons << endl;
+}
